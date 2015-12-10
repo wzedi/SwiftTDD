fb8ccf97a9eb162df87570e820ca078246f171de
Warrick Zedi <wzedi@cadreworks.com>
2015-12-11 06:22:42 +1100

    Initial Commit

---
+++ SwiftTDDTests/SwiftTDDTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftTDDTests.swift
+//  SwiftTDDTests
+//
+//  Created by Warrick Zedi on 11/12/2015.
+//  Copyright © 2015 CadreWorks Pty Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftTDD
+
+class SwiftTDDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
